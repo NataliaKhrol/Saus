a1827d079d03edcd2975e4a7f2d088a90dae9a34
Natalia Khrol <natalia_khrol@epam.com>
2026-02-04 19:20:21 +0300

    New check methods added

---
+++ src/test/java/tests/CartTest.java
@@ -0,0 +1,28 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+import static user.UserFactory.withAdminPermission;
+
+public class CartTest extends BaseTest {
+    final String goodsName = "Sauce Labs Onesie";
+
+    @Test
+    public void checkGoodsAdded() {
+        System.out.println("CartTest.correct !!!!! in thread: " + Thread.currentThread().getId());
+
+        loginPage.open();
+        loginPage.loginUser(withAdminPermission());
+        assertEquals(productsPage.checkTitleName(), "Products");
+
+        productsPage.addGoodsToCart(goodsName);
+        productsPage.switchToCart();
+
+        assertEquals(cartPage.checkTitleName(), "Your Cart");
+
+        assertFalse(cartPage.getProductsNames().isEmpty());
+        assertEquals(cartPage.getProductsNames().size(), 1);
+        assertTrue(cartPage.getProductsNames().contains(goodsName));
+    }
+}
